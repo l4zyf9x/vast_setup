72bb7c2c6abd4dcab58b8bf4f9267b4b7bbd3d54
trinhlq <trinhle.cse@gmail.com>
2022-12-13 15:52:20 +0700

    add setup gcsfuse

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
 echo "Running setup vast"
-export FINISHED_SETUP_VAST=true
+
+mkdir -p /usr/bin/gpg-agent
+sudo apt remove gpg
+sudo apt-get update -y
+sudo apt-get install -y gnupg1
+
+echo "Installing gcsfuse"
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install gcsfuse
+echo "Finish install gcsfuse"
+
+
 echo "Finished setup vast"
