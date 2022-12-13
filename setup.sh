e42b9fa290a294312117fc98fad9f6b540e26a96
trinhlq <trinhle.cse@gmail.com>
2022-12-13 16:27:49 +0700

    add yes to apt install

--- setup.sh
+++ setup.sh
@@ -2,15 +2,14 @@ echo "Running setup vast"
 
 mkdir -p /usr/bin/gpg-agent
 sudo apt remove gpg -y
-sudo apt-get update -y
 sudo apt-get install -y gnupg1
 
 echo "Installing gcsfuse"
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install gcsfuse
+sudo apt-get update -y
+sudo apt-get install gcsfuse -y
 echo "Finish install gcsfuse"
 
 
